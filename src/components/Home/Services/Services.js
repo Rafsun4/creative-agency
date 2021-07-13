64a7c7173aee850c0c3772bee461f03d6296c340
Rafsun4 <saifrafsun@gmail.com>
2021-07-13 16:49:56 +0600

    added skeleton

--- src/components/Home/Services/Services.js
+++ src/components/Home/Services/Services.js
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import Clients from './Clients';
 import ServiceCard from './ServiceCard';
-
+import MyLoader from '../../MyLoader';
 
 const Services = () => {
     const [service, setService] = useState([]);
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
         fetch('https://cryptic-savannah-96196.herokuapp.com/allServices')
-            .then(res => res.json())
-            .then(data => setService(data))
+        .then((res) => res.json())
+        .then((data) => {
+          setService(data);
+          setLoading(false);
+        });
     }, [])
     
     return (
@@ -18,6 +23,8 @@ const Services = () => {
                 <h2 className="text-center font-weight-bolder pt-4 pb-4">Provide awesome <span style={{ color: "#7AB259" }}>services</span></h2>
                 <div className="row d-flex justify-content-center mw-100 pt-4 pb-4 ml-2">
                         {
+                            loading ? (<MyLoader />)
+                            :
                             service.map(service => <ServiceCard service={service} key={service._id} />)
                         }
                 </div>
--- src/components/Home/Testimonials/Testimonial.js
+++ src/components/Home/Testimonials/Testimonial.js
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import Feedback from './Feedback';
+import MyLoader from '../../MyLoader';
 
 const Testimonial = () => {
     const [feedBack, setFeedBack] = useState([]);
+    const [loading, setLoading] =useState(true)   
+
     useEffect(() => {
         fetch('https://cryptic-savannah-96196.herokuapp.com/allReview')
             .then(res => res.json())
-            .then(data => setFeedBack(data))
+            .then((data) => {
+                setFeedBack(data);
+                setLoading(false);
+              });
     }, [])
 
 
@@ -15,6 +21,7 @@ const Testimonial = () => {
             <h2 className="text-center font-weight-bolder pt-4 pb-4">Clients <span style={{ color: "#7AB259" }}>Feedback</span></h2>
             <div className="row d-flex justify-content-center mw-100 pt-4 pb-4">
                 {
+                    loading ? (<MyLoader />):
                     feedBack.map(client => <Feedback client={client} key={client._id} />)
                 }
             </div>
